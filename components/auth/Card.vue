da7d901e1cfb736c44d7b8c107b6b400c2804b54
ilg2005 <igoldfarb10@gmail.com>
2022-07-25 20:43:20 +0300

    AuthCard

--- components/auth/Card.vue
+++ components/auth/Card.vue
@@ -1,99 +1,67 @@
 <template>
-  <v-form
-      ref="form"
-      v-model="valid"
-      lazy-validation
-  >
-    <v-text-field
-        v-model="name"
-        :counter="10"
-        :rules="nameRules"
-        label="Name"
-        required
-    ></v-text-field>
-
-    <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-    ></v-text-field>
-
-    <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
-        label="Item"
-        required
-    ></v-select>
-
-    <v-checkbox
-        v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
-        required
-    ></v-checkbox>
-
-    <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        @click="validate"
+  <v-card class="px-12 pb-12 ma-auto w-50">
+    <v-card-title class="text-h4 mb-5">Login</v-card-title>
+    <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
     >
-      Validate
-    </v-btn>
 
-    <v-btn
-        color="error"
-        class="mr-4"
-        @click="reset"
-    >
-      Reset Form
-    </v-btn>
+      <v-text-field
+          v-model="fields.email"
+          :rules="rules.email"
+          label="E-mail"
+          required
+      ></v-text-field>
 
-    <v-btn
-        color="warning"
-        @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
-  </v-form>
+      <v-text-field
+          v-model="fields.password"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="show1 ? 'text' : 'password'"
+          @click:append="show1 = !show1"
+          :rules="rules.password"
+          :counter="8"
+          label="Password"
+          required
+      ></v-text-field>
+
+      <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="validate"
+      >
+        Login
+      </v-btn>
+
+    </v-form>
+  </v-card>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-    ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-    ],
-    select: null,
-    items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-    ],
-    checkbox: false,
-  }),
+<script setup>
 
-  methods: {
-    validate () {
-      this.$refs.form.validate()
-    },
-    reset () {
-      this.$refs.form.reset()
-    },
-    resetValidation () {
-      this.$refs.form.resetValidation()
-    },
-  },
+const fields = reactive({
+  email: '',
+  password: ''
+})
+
+const rules = {
+  email: [
+    v => !!v || 'E-mail is required',
+    v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+  ],
+  password: [
+    v => !!v || 'Password is required',
+    v => (v && v.length >= 8) || 'Password must be not less than 8 characters',
+  ]
 }
+const valid = ref(true);
+const show1 = ref(false);
+const form = ref();
+const validate = () => form.value.validate();
+
 </script>
+
+<style scoped>
+
+</style>
